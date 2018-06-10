1cedd3500d4156e114782010f88830219c32983e
Alex <dogrock2@Alexanders-MacBook-Pro.local>
2018-06-10 11:38:38 -0400

    changed scope

--- js/script.js
+++ js/script.js
@@ -1,69 +1,81 @@
 "use strict";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-	let playerTurn = true;
-	let positions = [false,false,false,false,false,false,false,false,false];
-    let combo1 = [1,4,7,1,2,3,3,1];
-	let combo2 = [2,5,8,4,5,6,5,5];
-	let combo3 = [3,6,9,7,8,9,7,9];
-	let vals = [];
-	let winner = false;
-	let clickCnt = 0;
-	
+	const init = {
+		combo1: [1, 4, 7, 1, 2, 3, 3, 1],
+		combo2: [2, 5, 8, 4, 5, 6, 5, 5],
+		combo3: [3, 6, 9, 7, 8, 9, 7, 9],
+		playerTurn: true,
+		positions: [],
+		vals: [],
+		winner: false,
+		clickCnt: 0,
 
-	$("#mainContainer > .boxesCnt > div").click(function() {
-		if(!winner){
-			let clickedID = $(this).attr("id");
-			let clickedNum = $(this).attr("attrNum");
-			let pos =  parseInt(clickedNum) - 1;
-		  
-			if (!positions[pos]){
-			    clickCnt++;
-				if (playerTurn) {				
+		promptNew: (plyrWinnr) => {
+			$("#msg").html(plyrWinnr);
+			$("#msg").append("<button class='btn btn-info' id='againBtn'>PlayAgain?</button>");
+		},
+
+		resetFnc: function () {
+			$("#msg").html("");
+			this.winner = false;
+			this.positions = [false, false, false, false, false, false, false, false, false];
+			this.playerTurn = true;
+			this.vals = [];
+			this.clickCnt = 0;
+			for (let i = 1; i <= 9; i++)
+				$(`#box${i}`).empty();
+		},
+
+	};
+
+	$("#mainContainer > .boxesCnt > div").click(function () {
+		if (!init.winner) {
+			const clickedID = $(this).attr("id");
+			const clickedNum = $(this).attr("attrNum");
+			const positions = init.positions;
+			const combo1 = init.combo1;
+			const combo2 = init.combo2;
+			const combo3 = init.combo3;
+			let pos = parseInt(clickedNum) - 1;
+			let vals = init.vals;
+
+			if (!positions[pos]) {
+				init.clickCnt++;
+				if (init.playerTurn) {
 					$("#" + clickedID).html('<img class="imgXO" src="X.png" attrVal="x"/>');
 					positions[pos] = true;
 					vals[pos] = "x";
-					playerTurn = !playerTurn;
-				} else {          
+					init.playerTurn = !init.playerTurn;
+				} else {
 					$("#" + clickedID).html('<img class="imgXO" src="o.png" attrVal="o"/>');
 					positions[pos] = true;
 					vals[pos] = "o";
-					playerTurn = !playerTurn;
-				} 
+					init.playerTurn = !init.playerTurn;
+				}
 				for (let i = 0; i < 9; i++) {
-					if (vals[combo1[i] - 1] === "x" && vals[combo2[i] - 1] === "x" && vals[combo3[i] - 1] === "x"){   
-						winner = true;
-						promptNew('PLAYER  1  (X) WINS!    ');
+					if (vals[combo1[i] - 1] === "x" && vals[combo2[i] - 1] === "x" && vals[combo3[i] - 1] === "x") {
+						init.winner = true;
+						init.promptNew('PLAYER  1  (X) WINS!    ');
 					}
-					if (vals[combo1[i] - 1] === "o" && vals[combo2[i] - 1] === "o" && vals[combo3[i] - 1] === "o"){
-						winner = true;
-						promptNew('PLAYER  2  (O) WINS!    ');
+					if (vals[combo1[i] - 1] === "o" && vals[combo2[i] - 1] === "o" && vals[combo3[i] - 1] === "o") {
+						init.winner = true;
+						init.promptNew('PLAYER  2  (O) WINS!    ');
 					}
 				} //ends winCheck for Loop
-				if(clickCnt === 9 && winner === false){
-					winner = true;
-					promptNew('Game TIED!    ');
+				if (init.clickCnt === 9 && init.winner === false) {
+					init.winner = true;
+					init.promptNew('Game TIED!    ');
 				}
-			}//ends position if
-		}//ends winner if
+			} //ends position if
+		} //ends winner if
 	}); //ends child on click
-	
-	$('#botContainer').on('click','#againBtn', function(){
-		$("#msg").html("");
-		winner = false;
-		positions = [false,false,false,false,false,false,false,false,false];
-		playerTurn = true;
-		vals = [];
-		clickCnt = 0;
-		for(let i = 1; i <= 9; i++)
-		  $(`#box${i}`).empty();
-	});
-	
-	function promptNew(plyrWinnr){
-		$("#msg").html(plyrWinnr);
-		$("#msg").append("<button class='btn btn-info' id='againBtn'>PlayAgain?</button>");
-	}
 
+	$('#botContainer').on('click', '#againBtn', () => {
+		init.resetFnc();
+	});
+
+	init.resetFnc();
 
 }); //ends document ready
